bf86f4064bc3bda8767ec7346296125b0260f565
ASHUTOSH SATPATHY <147174405+DevAshuTosh10@users.noreply.github.com>
2025-02-06 23:55:15 +0530

    Update App.jsx

--- App.jsx
+++ App.jsx
@@ -1,7 +1,7 @@
 // src/App.jsx
 import InterestCalculator from './components/InterestCalculator';
 
-function App() {
+function App(){
   return (
     <div className="flex items-center justify-center min-h-screen bg-gray-100 p-4">
       <InterestCalculator />
